477a681bf17fdc3d9b38463c1d825d51f56301ee
Clahryott <ck.truscott@gmail.com>
2023-02-26 20:33:50 +0800

    uncommented form onsubmit

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -54,38 +54,38 @@ function LoginForm() {
     }
 };
 
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <div>
-//         <label htmlFor="username">Username:</label>
-//         <input
-//           type="text"
-//           id="username"
-//           onChange={handleChange}
-//           placeholder="Enter username"
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="password">Password:</label>
-//         <input
-//           type="password"
-//           id="password"
-//           onChange={handleChange}
-//           placeholder="Enter username"
-//         />
-//       </div>
-//       <div>
-//         <label htmlFor="password">Password:</label>
-//         <input
-//           type="password"
-//           id="password"
-//           onChange={handleChange}
-//           placeholder="Password"
-//           />
-//       </div>
-//       <button type="submit">Login</button>
-//     </form>
-//   );
-// }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          onChange={handleChange}
+          placeholder="Enter username"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={handleChange}
+          placeholder="Enter username"
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={handleChange}
+          placeholder="Password"
+          />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
 
 export default LoginForm;
