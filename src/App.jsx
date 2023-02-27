e902466d79b4d3d3036d30f0c122ae37848863c2
Clahryott <ck.truscott@gmail.com>
2023-02-27 11:32:12 +0800

    commented out footer and uncommented createprojectpage

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProjectPage from "./pages/ProjectPage";
-// import CreateProjectPage from "./pages/CreateProjectPage";
+import CreateProjectPage from "./pages/CreateProjectPage";
 import PledgePage from "./pages/PledgePage";
 import UserPage from "./pages/UserPage";
 
 
 // Components
 import Nav from "./components/Nav/Nav";
-// import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer"; to be created
 
 // CSS
 import "./App.css";
@@ -24,7 +24,7 @@ const [loggedIn, setLoggedIn] = useState(window.localStorage.getItem("token") !=
     <>
       <Nav loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Outlet context={[loggedIn, setLoggedIn]} />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
@@ -35,11 +35,11 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/project/:id", element: <ProjectPage /> },
-      { path: "/about", element: <AboutPage /> },
-      { path: "/contact", element: <ContactPage /> },
+      // { path: "/about", element: <AboutPage /> },
+      // { path: "/contact", element: <ContactPage /> },
       { path: "/login", element: <LoginPage /> },
       { path: "/pledges", element: <PledgePage /> },
-      { path: "/register", element: <RegistrationPage /> },
+      // { path: "/register", element: <RegistrationPage /> },
       { path: "/create-project", element: <CreateProjectPage /> },
       { path: "/user/:id", element: <UserPage /> },  
     ],
